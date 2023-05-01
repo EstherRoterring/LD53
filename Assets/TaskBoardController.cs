64ad0a90910db6d42504aaf69758325a35e8e222
Frithjof Petrick <fpetrick@ebay.com>
2023-05-01 17:40:54 +0200

    work board

--- Assets/TaskBoardController.cs
+++ Assets/TaskBoardController.cs
@@ -12,5 +12,23 @@ using UnityEngine.UI;
 
 public class TaskBoardController : MonoBehaviour
 {
+    public TextMeshProUGUI text;
     
+    private void Start()
+    {
+        this.transform.localPosition = new Vector3(0, 0.3f, -3);
+    }
+
+    private void Update()
+    {
+        var str = "";
+        foreach (var activeTask in OfficeController.INSTANCE.activeTaskSequences)
+        {
+            if (!activeTask.bonus)
+            {
+                str += activeTask.GetText() + "\n";
+            }
+        }
+        text.SetText(str);
+    }
 }
